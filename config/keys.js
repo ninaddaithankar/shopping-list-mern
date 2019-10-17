8243465e8815d767863a6ac8e926b2b5926f3ff7
Ninad Daithankar <ninad.nd15@gmail.com>
2019-10-18 00:05:02 +0530

    Added model schema

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://ninad:ninad123@mycluster-36w9a.mongodb.net/test?retryWrites=true&w=majority"
+};
--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const keys = require("./config/keys");
 
 const app = new express();
+const db = keys.mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("Connected");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+
+app.listen(PORT, console.log("Server Running"));
