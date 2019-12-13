9046d4f909d2fe8b5ed85a22a9a0929e97c662eb
Ninad Daithankar <ninad.nd15@gmail.com>
2019-12-13 23:07:44 +0530

    Version 1.0.0

--- client/src/components/ItemModel.js
+++ client/src/components/ItemModel.js
@@ -11,7 +11,6 @@ import {
 } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import uuid from 'uuid';
 
 class ItemModel extends Component {
 	state = {
@@ -33,7 +32,6 @@ class ItemModel extends Component {
 		e.preventDefault();
 
 		const newItem = {
-			id: uuid(),
 			name: this.state.name
 		};
 
--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
-const items = require("./routes/api/items");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const items = require('./routes/api/items');
+const path = require('path');
 
 const app = new express();
 const db = keys.mongoURI;
 
 mongoose
-  .connect(db)
-  .then(() => {
-    console.log("Connected");
-  })
-  .catch(err => {
-    console.log(err);
-  });
+	.connect(db)
+	.then(() => {
+		console.log('Connected');
+	})
+	.catch(err => {
+		console.log(err);
+	});
 
-const PORT = process.env.PORT || 5000;
-
-app.use("/api/items", items);
+app.use('/api/items', items);
 
 app.use(express.json());
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server Running at ${PORT}`));
